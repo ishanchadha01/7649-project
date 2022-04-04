516ec7faaa355d0500d15d38f76f7e6529834763
Mudit Gupta <muditg317@gmail.com>
2022-04-04 16:12:58 -0400

    impl rrt

--- main/node.py
+++ main/node.py
@@ -1,5 +1,8 @@
 class Node:
-    def __init__(self, coord=tuple, parent=None) -> None:
+    def __init__(self, coord: tuple, parent: 'Node'=None) -> None:
         self.parent = parent
         self.coord = coord
         self.children = []
+    
+    def coord_copy(self):
+        return Node(self.coord)
