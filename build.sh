c70b57d0666eb94631e4c13609ffec1dee1097d7
Mudit Gupta <muditg317@gmail.com>
2022-04-10 04:28:13 -0400

    make run script work with main.world file yay

--- build.sh
+++ build.sh
@@ -1,18 +1,25 @@
 set -e
 
 rebuild=false
+target=all
 while [[ $# -gt 0 ]]
 do
     case $1 in
         -h|--help)
             echo "Usage: configure.sh [OPTIONS]"
             echo "Options:"
-            echo "  -h, --help                 Print this help message"
-            echo "  -r, --rebuild              Force rebuild by deleting the build directory"
+            echo "  -h, --help                      Print this help message"
+            echo "  -r, --rebuild                   Force rebuild by deleting the build directory"
+            echo "  -t, --target [build target]     Build the specified target"
             exit 0
             ;;
         -r|--re-build)
             rebuild=true
+            shift
+            ;;
+        -t|--target)
+            target=$2
+            shift
             shift
             ;;
         *)
@@ -28,14 +35,14 @@ proj_dir=$(pwd)
 build_dir=$proj_dir/build
 
 if [ ! -d "$build_dir" ]; then
-   echo "'$DIR' not found! Configuring CMake..."
+   echo "'$build_dir' not found! Configuring CMake..."
    ./configure.sh
 else
    if [ "$rebuild" = true ]; then
-      echo "Deleting '$DIR'..."
+      echo "Deleting '$build_dir'..."
       rm -rf $build_dir
       ./configure.sh
    fi
 fi
 
-$_cmake --build "$build_dir" --config RelWithDebInfo --target all -j 14 --
+$_cmake --build "$build_dir" --config RelWithDebInfo --target $target -j 14 --
