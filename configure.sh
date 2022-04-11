499a2a90e52417cf8841970fc8b4886edb10ea4d
Ishan Chadha <ishanchadha01@gmail.com>
2022-04-11 15:46:56 -0400

    mac build config for sim

--- configure.sh
+++ configure.sh
@@ -28,8 +28,11 @@ build_type=Release
 build_type=RelWithDebInfo
 
 _cmake=/usr/bin/cmake
+[ ! -d "/usr/bin/cmake" ] && _cmake=/usr/local/bin/cmake
 _cc=/bin/gcc
+[ ! -d "/bin/gcc" ] && _cc=/usr/bin/clang
 cxx=/bin/g++
+[ ! -d "/bin/g++" ] && cxx=/usr/bin/clang++
 # toolchain=$HOME/application-installs/vcpkg/scripts/buildsystems/vcpkg.cmake
 
 proj_dir=$(pwd)
@@ -38,4 +41,6 @@ build_dir=$proj_dir/build
 if [ "$rebuild" = true ]; then
     rm -rf $build_dir
 fi
+
+echo $_cmake && echo $_cc && echo $cxx
 $_cmake --no-warn-unused-cli -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=$build_type -DCMAKE_C_COMPILER:FILEPATH=$_cc -DCMAKE_CXX_COMPILER:FILEPATH=$cxx -DCMAKE_TOOLCHAIN_FILE:FILEPATH=$toolchain "-S$proj_dir" "-B$build_dir" -G "Unix Makefiles"
