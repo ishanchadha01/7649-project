12174bbc9fae92167b9f5eac6c80166e820689df
Mudit Gupta <muditg317@gmail.com>
2022-04-21 17:12:22 -0400

    ignore devel and install

--- devel/local_setup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-# generated from catkin/cmake/template/local_setup.sh.in
-
-# since this file is sourced either use the provided _CATKIN_SETUP_DIR
-# or fall back to the destination set at configure time
-: ${_CATKIN_SETUP_DIR:=/mnt/c/Users/mudit/Main_Drive/2019-2023GT/2021-2022GTyr3/Spring 2022/CS7649-RIP/Final Project - RRTx variant/Field-Aided-RRT/devel}
-CATKIN_SETUP_UTIL_ARGS="--extend --local"
-. "$_CATKIN_SETUP_DIR/setup.sh"
-unset CATKIN_SETUP_UTIL_ARGS
