d55767a48fc749a67ac942cb7292340347bcf776
Ishan Chadha <ishanchadha01@gmail.com>
2022-04-24 14:33:37 -0400

    basic rrt

--- python/PartiallyObservablePlanner.py
+++ python/PartiallyObservablePlanner.py
@@ -1,14 +1,15 @@
 from abc import ABC, abstractmethod
+from copy import deepcopy
 from typing import Generic, TypeVar
-from main.node import Node
-from main.world import World, Coord
+from node import Node
+from world import World, Coord
 
 class PartiallyObservablePlanner(ABC):
-  def __init__(self, world: World, x_start: Coord, x_goal: Coord) -> None:
+  def __init__(self, world: World, x_start: Node, x_goal: Node) -> None:
     self.world = world
     self.x_start = x_start
     self.x_goal = x_goal
-    self.curr_pos = x_start
+    self.curr_pos = deepcopy(x_start)
 
   @abstractmethod
   def step(self) -> Coord:
