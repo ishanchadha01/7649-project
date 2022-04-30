41be6201662fbf0b9dc0d2d6c6568528f7e43850
Mudit Gupta <muditg317@gmail.com>
2022-04-29 22:13:12 -0400

    impl rrt* in farrt-star - currently buggy

---
+++ src/farrt/farrt-star.py
@@ -0,0 +1,284 @@
+import argparse
+import os
+from copy import deepcopy
+import random
+import math
+import shutil
+import tkinter as tk
+import numpy as np
+from scipy.spatial import KDTree
+from queue import PriorityQueue
+from matplotlib import pyplot as plt
+
+from shapely.geometry.base import BaseGeometry
+from shapely.geometry import Point, LineString, MultiPoint
+from shapely.ops import nearest_points
+
+from farrt.PartiallyObservablePlanner import PartiallyObservablePlanner
+from farrt.node import Node
+from farrt.plot import plot_polygons, plot_planner
+from farrt.world import World
+from farrt.utils import multipoint_without, shapely_edge
+
+import imageio
+
+vertex_t = tuple[float,float]
+edge_t = tuple[vertex_t,vertex_t]
+
+class FARRT(PartiallyObservablePlanner):
+
+  def __init__(self, *args, **kwargs) -> None:
+    super().__init__(*args, **kwargs)
+    self.iters = kwargs.get('iters', 1000)
+    self.eps = kwargs.get('eps', .01)
+
+    self.detected_obstacles = BaseGeometry()
+    self.steer_distance = kwargs.get('max_step_length', self.vision_radius / 3)
+
+
+    self.x_start_pt = self.x_start.coord
+    self.x_goal_pt = self.x_goal.coord
+
+    self.farrt_tree = MultiPoint()
+    self.farrt_vertices: set[vertex_t] = set()
+    self.farrt_edges: set[edge_t] = set()
+    self.parent_map: dict[vertex_t, vertex_t] = {}
+    self.cost_to_reach: dict[vertex_t, float] = {}
+
+    self.goal_reached_thresh = 1
+    self.built_tree = False
+
+
+  def handle_new_obstacles(self, new_obstacles: BaseGeometry) -> None:
+    if not self.built_tree:
+      self.do_first_plan()
+      return
+    path = [self.curr_pos.coord] + [node.coord for node in self.planned_path]
+    if len(path) <= 1 or LineString(path).intersects(new_obstacles):
+      print('Path is inconsistent with new obstacles')
+      intersections = None
+      if len(path) > 1:
+        # print(LineString(path))
+        intersections = LineString(path).intersection(new_obstacles)
+        # print(intersections)
+        # print(new_obstacles)
+      self.plan(draw_intersections=intersections)
+
+  def handle_deleted_obstacles(self, deleted_obstacles: BaseGeometry) -> None:
+      return super().handle_deleted_obstacles(deleted_obstacles)
+
+  def update_plan(self) -> None:
+      return super().update_plan()
+
+  def do_first_plan(self) -> None:
+    final_pt,final_cost = self.build_farrt_tree()
+    self.built_tree = True
+    self.planned_path = self.extract_path(final_pt)
+
+  def plan(self, **kwargs):
+    print('Planning...')
+    self.render(draw_world_obstacles=False, save_frame=True, **kwargs)
+    self.build_farrt_tree(self.x_goal, self.curr_pos)
+    self.planned_path = self.extract_path(self.x_goal)
+    print('Planning complete')
+    # self.render()
+
+  def sample_free(self, goal_pt: Point) -> Point:
+    if random.random() < self.eps: # some % chance of returning goal node
+      return goal_pt
+    rand_pos = self.world.random_position()
+    while rand_pos is None or self.detected_obstacles.contains(rand_pos):
+      rand_pos = self.world.random_position()
+    return rand_pos
+
+  def find_nearest(self, x_rand: Point) -> Point:
+    nearest_geoms = nearest_points(self.farrt_tree, x_rand)
+    nearest_pt = nearest_geoms[0]
+    return nearest_pt
+
+  def steer(self, x_nearest: Point, x_rand: Point) -> Point:
+    dist = x_nearest.distance(x_rand)
+    if dist == 0:
+      return x_rand
+    factor = min(1, self.steer_distance / dist)
+    newCoord = shapely_edge(x_nearest,x_rand).interpolate(factor, normalized=True)
+    return newCoord
+
+  def edge_obstacle_free(self, x_nearest: Point, x_new: Point) -> bool:
+    return not self.detected_obstacles.intersects(shapely_edge(x_nearest,x_new))
+
+  def find_nearby_pts(self, x_new: Point) -> MultiPoint:
+
+    def find_ball_radius():
+      unit_volume = math.pi
+      num_vertices = len(self.farrt_vertices)
+      dimensions = len(self.world.dims)
+      minx,miny,maxx,maxy = self.world.getBounds()
+      gamma = (2**dimensions)*(1.0 + 1.0/dimensions) * (maxx - minx) * (maxy - miny)
+      ball_radius = min(
+        ((gamma/unit_volume) * math.log(num_vertices) / num_vertices)**(1.0/dimensions),
+        self.steer_distance
+      )
+      return ball_radius
+    
+    nearby_points = self.farrt_tree.intersection(x_new.buffer(find_ball_radius()))
+
+    if nearby_points.is_empty:
+      return MultiPoint()
+    elif isinstance(nearby_points, MultiPoint):
+      return nearby_points
+    elif isinstance(nearby_points, Point):
+      return MultiPoint([nearby_points])
+    else:
+      return MultiPoint()
+
+  def get_min_cost_point(self, nearby_pts: MultiPoint, x_nearest: Point, x_new: Point) -> Point:
+    """
+    Returns the point in nearby_pts that has the minimum cost to get to x_new
+    defaults to x_nearest
+    """
+    min_point = x_nearest
+    min_cost = self.get_cost_to_reach(x_nearest) + self.get_edge_cost(x_nearest, x_new)
+    for point in nearby_pts.geoms:
+      if self.edge_obstacle_free(point, x_new):
+        temp_cost = self.get_cost_to_reach(point) + self.get_edge_cost(point, x_new)
+        if temp_cost < min_cost:
+          min_point = point
+          min_cost = temp_cost
+    return min_point,min_cost
+
+  def add_start_vertex(self, x_start: Point) -> None:
+    self.farrt_tree = self.farrt_tree.union(x_start)
+
+    vtx = self.as_vertex(x_start)
+
+    self.farrt_vertices.add(vtx)
+    
+    self.set_cost_to_reach(x_start, 0)
+
+  def add_vertex(self, pt: Point, parent: Point, cost: float) -> None:
+    self.farrt_tree = self.farrt_tree.union(pt)
+
+    vtx = self.as_vertex(pt)
+    parent_vtx = self.as_vertex(parent)
+
+    self.farrt_vertices.add(vtx)
+    self.farrt_edges.add((parent_vtx,vtx))
+
+    self.set_parent(pt, parent)
+    self.set_cost_to_reach(pt, cost)
+
+  def reassign_parent(self, pt: Point, parent: Point, cost: float) -> None:
+    prev_parent = self.get_parent(pt)
+
+    vtx = self.as_vertex(pt)
+    old_parent_vtx = self.as_vertex(prev_parent)
+    new_parent_vtx = self.as_vertex(parent)
+
+    self.farrt_edges.discard((old_parent_vtx,vtx))
+    self.farrt_edges.discard((vtx,old_parent_vtx))
+    self.farrt_edges.add((new_parent_vtx,vtx))
+    
+    self.set_parent(pt, parent)
+    self.set_cost_to_reach(pt, cost)
+
+  def do_rrtstar_rewiring(self, nearby_pts: MultiPoint, x_min: Point, x_new: Point) -> None:
+    """
+    Rewire the edges of the tree to connect x_new first if it is closer than the current parent
+    Discards edges from points in nearby_pts if they can be improved by going thorugh x_new
+      Then adds an edge from x_new to the point in nearby_pts (and update parent)
+    """
+    for x_nearby in multipoint_without(nearby_pts, x_min).geoms:
+      if self.edge_obstacle_free(x_nearby, x_new):
+        cost_with_new = self.get_cost_to_reach(x_new) + self.get_edge_cost(x_nearby, x_new)
+        if self.get_cost_to_reach(x_nearby) > cost_with_new:
+            self.reassign_parent(pt=x_nearby, parent=x_new, cost=cost_with_new)
+
+  def reached_goal(self, x_new: Point) -> bool:
+    return x_new.distance(self.x_goal) < self.goal_reached_thresh
+
+  def build_farrt_tree(self) -> None:
+    self.farrt_tree.empty() 
+    self.farrt_vertices.clear()
+    self.farrt_edges.clear()
+    self.add_start_vertex(self.x_start_pt)
+
+    final_pt = None
+    final_pt_cost = float('inf')
+
+    for i in range(self.iters):
+      x_rand = self.sample_free(self.x_goal_pt)
+      x_nearest = self.find_nearest(x_rand)
+      x_new = self.steer(x_nearest, x_rand)
+      if self.edge_obstacle_free(x_nearest, x_new):
+        # find nearby points to the new point
+        nearby_points = self.find_nearby_pts(x_new)
+
+        # get the minimum point from the set
+        x_min,min_cost = self.get_min_cost_point(nearby_points, x_nearest, x_new)
+
+        # add the new point to the tree
+        self.add_vertex(pt=x_new,parent=x_min,cost=min_cost)
+
+        # Main difference between RRT and RRT*, modify the points in the nearest set to optimise local path costs.
+        self.do_rrtstar_rewiring(nearby_points, x_min, x_new)
+
+        if self.reached_goal(x_new):
+          if self.built_tree: # subsequent runs should just terminate once goal is reached
+            final_pt = x_new
+            break
+          else: # keep searching and update the shortest path
+            if min_cost < final_pt_cost:
+              final_pt = x_new
+              final_pt_cost = min_cost
+    return final_pt,final_pt_cost
+
+  def extract_path(self, final_pt: Point) -> list[Node]:
+    curr = final_pt
+    path: list[Point] = []
+    while curr != self.curr_pos.coord:
+      path.append(curr)
+      curr = self.get_parent(curr)
+    # path.append(self.curr_pos.coord)
+    path.reverse() # curr pos first
+    node_path = []
+    for i,pt in enumerate(path):
+      parent = self.curr_pos if i == 0 else node_path[i-1]
+      node_path.append(Node(pt,parent))
+    return node_path
+
+  def as_vertex(self, pt: Point) -> vertex_t:
+    return pt.coords[0]
+
+  def get_parent(self, point: Point) -> Point:
+    return Point(self.parent_map[point.coords[0]])
+
+  def set_parent(self, point: Point, parent: Point) -> None:
+    self.parent_map[point.coords[0]] = parent.coords[0]
+
+  def get_cost_to_reach(self, point: Point) -> float:
+    return self.cost_to_reach[point.coords[0]]
+
+  def set_cost_to_reach(self, point: Point, cost: float) -> None:
+    self.cost_to_reach[point.coords[0]] = cost
+
+  def get_edge_cost(self, point1: Point, point2: Point) -> float:
+    return point1.distance(point2)
+
+  def get_render_kwargs(self) -> dict:
+    return {
+      'rrt_tree': self.farrt_tree,
+      'rrt_parents': self.parent_map
+    }
+
+
+if __name__=='__main__':
+  # extract run args run_count
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-rc', type=int, dest='run_count')
+  args = parser.parse_args()
+  filtered_args = {k: v for k, v in vars(args).items() if v is not None}
+
+  world = World()
+  farrt = FARRT(world=world, x_start=Node(world.random_position(not_blocked=True)), x_goal=Node(world.random_position(not_blocked=True)), gui=True, **filtered_args)
+  farrt.run()
--- src/farrt/plot.py
+++ src/farrt/plot.py
@@ -3,7 +3,7 @@ from matplotlib.path import Path
 from matplotlib.patches import PathPatch
 from matplotlib.collections import PatchCollection
 
-from shapely.geometry import Polygon, Point
+from shapely.geometry import Polygon, Point, MultiPoint
 from shapely.geometry.base import BaseGeometry, BaseMultipartGeometry
 import matplotlib.pyplot as plt
 from farrt.node import Node
@@ -40,20 +40,29 @@ def plot_line(ax, start: Point, end: Point, **kwargs):
   else:
     raise TypeError('plot_line() expect two Points, but got a {} and {}'.format(type(start), type(end)))
 
-def plot_points(points: list, ax=None, **kwargs):
+def plot_points(points: list, parents_map:dict = None, ax=None, **kwargs):
   if ax is None:
     fig,ax = plt.subplots()
   else:
     fig=None
-  if len(points) > 0 and isinstance(points[0], Node):
+  if isinstance(points, MultiPoint):
+    points = points.geoms
+  if (parents_map is not None) or (len(points) > 0 and isinstance(points[0], Node)):
     kwargs_without_marker = {k:v for k,v in kwargs.items() if k != 'marker'}
     if 'edgecolor' in kwargs:
       kwargs_without_marker['color'] = kwargs_without_marker.pop('edgecolor')
       # kwargs_without_marker.pop('edgecolor')
       kwargs.pop('edgecolor')
-    point: Node
     for point in points:
-      if point.parent is not None:
+      parent = None
+      coord = None
+      if isinstance(point, Node):
+        parent = point.parent
+        coord = point.coord
+      elif parents_map is not None:
+        parent = parents_map[point]
+        coord = point
+      if coord is not None and parent is not None:
         plot_line(ax, point.parent.coord, point.coord, marker='', linestyle='-', linewidth=kwargs_without_marker.pop('linewidth', 2), **kwargs_without_marker)
   if 'edgecolor' in kwargs and len(points) > 0:
     print('edgecolor not handled!')
@@ -115,14 +124,14 @@ def plot_world(world: World, draw_obstacles: bool = True, **kwargs):
   border_color = kwargs.pop('border_color', 'red')
   obstacle_color = kwargs.pop('obstacle_color', 'black')
   obstacle_edge_color = kwargs.pop('obstacle_edge_color', 'red')
-  plot_polygon(ax, world.getBoundingPoly(), facecolor=background_color, edgecolor=border_color, **kwargs)
+  plot_polygon(ax, world.getBoundingPolygon(), facecolor=background_color, edgecolor=border_color, **kwargs)
   if draw_obstacles:
     plot_polygons(world.obstacles.geoms, dims=world.dims, ax=ax, facecolor=obstacle_color, edgecolor=obstacle_edge_color, **kwargs)
   ax.set_xlim([0,world.dims[0]])
   ax.set_ylim([0,world.dims[1]])
   return fig,ax
 
-def plot_planner(world: World = None, curr_pos: Node = None, goal:Node = None, observations: BaseGeometry = None, rrt_tree:list[Node] = None, planned_path:list[Node] = None, **kwargs):
+def plot_planner(world: World = None, curr_pos: Node = None, goal:Node = None, observations: BaseGeometry = None, rrt_tree:list[Node] = None, rrt_parents:dict = None, planned_path:list[Node] = None, **kwargs):
   if 'fig_ax' in kwargs:
     fig,ax = kwargs.pop('fig_ax')
   else:
@@ -147,7 +156,7 @@ def plot_planner(world: World = None, curr_pos: Node = None, goal:Node = None, o
       plot_points(list(draw_intersections.coords), ax=ax, marker="o", markersize=10, markeredgecolor="purple", markerfacecolor="purple", linewidth=3)
   
   if rrt_tree is not None:
-    plot_points(rrt_tree, ax=ax, marker=".", markersize=3, markeredgecolor="yellow", markerfacecolor="yellow", edgecolor='yellow', linewidth=1)
+    plot_points(rrt_tree, parents_map=rrt_parents, ax=ax, marker=".", markersize=3, markeredgecolor="yellow", markerfacecolor="yellow", edgecolor='yellow', linewidth=1)
   if planned_path is not None:
     plot_points(planned_path, ax=ax, marker=".", markersize=5, markeredgecolor="orange", markerfacecolor="orange", edgecolor='orange', linewidth=3)
   
